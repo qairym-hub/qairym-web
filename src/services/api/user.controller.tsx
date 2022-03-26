6fbb029b137333cee2a2c0cd387f9a34f4c5b54a
khanZhomart <jtalgatulu@yandex.kz>
2022-03-26 17:38:42 +0600

    Реализовал методы login и create для взаимодействия с API

--- src/services/api/user.controller.tsx
+++ src/services/api/user.controller.tsx
@@ -1,17 +1,43 @@
-import axios from 'axios'
-import { ArrowFunction } from 'typescript';
+import axios, { AxiosError } from 'axios'
 
 class UserController {
-    
-    public static async login(username: String, password: String, callback: Function) {
+
+    public async login(username: String, password: String, callback: any) {
+        try {
+            const response = await axios.post(
+                `${process.env.REACT_APP_BASE_URL}/api/user/login?username=${username}&password=${password}`
+            )
+
+            return callback(response.data)
+        } catch (error: AxiosError | any) {
+            if (axios.isAxiosError(error))
+                return callback(null, error)
+
+            return console.error('Stock error', error)
+        }
+    }
+
+    public async create(user: Object, callback: any) {
+        try {
+            const response = await axios.post(
+                `${process.env.REACT_APP_BASE_URL}/api/user/register`,
+                user
+            )
+
+            return callback(response.data)
+        } catch (error: AxiosError | any) {
+            if (axios.isAxiosError(error))
+                return callback(null, error)
+
+            return console.error('Stock error', error)
+        }
+    }
+
+    public async getById(userId: Number, callback: any) {
 
     }
 
-    public static async create(user: Object, callback: Function) {
+    public async loadByUsername(username: String, token: String) {
 
-    }
-
-    public static async loadByUsername(username: String, token: String) {
-        
     }
 }
