ceaa1cad7f134eff98db4046d8c15d73847ab675
khanZhomart <jtalgatulu@yandex.kz>
2022-03-26 23:42:52 +0600

    Изменил вёрстку стандартной кнопки для лучшей гибкости

--- src/components/buttons/DefaultButton.tsx
+++ src/components/buttons/DefaultButton.tsx
@@ -2,33 +2,41 @@ import React from 'react'
 import { Button } from 'react-bootstrap'
 
 interface ButtonComponentProps {
+  className?: string,
   text: string,
+  fluid?: boolean,
   color?: string,
   icon?: React.ReactNode,
   loading?: boolean,
   onClick?: any
 }
 
-const DefaultButton = ({ text, color = 'primary', icon, loading, onClick }: ButtonComponentProps) => {
+const DefaultButton: React.FunctionComponent<ButtonComponentProps> = (
+  {
+    className,
+    fluid,
+    text,
+    color = 'primary',
+    icon,
+    loading,
+    onClick
+  }
+) => {
 
   return (
     <>
       <Button
-        className="d-flex align-items-center border rounded-extra"
+        className={`border rounded-extra ${fluid && "w-100"} ${className}`}
         variant={color}
         onClick={onClick}
         disabled={loading}
       >
-        <span className="d-flex px-5">
-          {icon && (
-            <div className="d-flex align-items-center">
-              {icon}
-            </div>
-          )}
+        <div className="d-flex align-items-center justify-content-center px-3">
+          {icon}
           <span className="text-roboto-500 px-2">
             {text}
           </span>
-        </span>
+        </div>
       </Button>
     </>
   )
