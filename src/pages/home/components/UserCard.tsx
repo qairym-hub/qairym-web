39d8ecc09ef329762195dc179667067fba69dda4
khanZhomart <jtalgatulu@yandex.kz>
2022-04-21 17:40:03 +0600

    Работа над размерами иконок

--- src/pages/home/components/UserCard.tsx
+++ src/pages/home/components/UserCard.tsx
@@ -15,14 +15,14 @@ const UserCard: React.FunctionComponent<UserCardProps> = (
     return (
         <>
             <div
-                style={{ width: "300px", paddingLeft: "20px", paddingRight: "20px" }}
+                style={{ width: "250px", paddingLeft: "20px", paddingRight: "20px" }}
                 className="d-flex border rounded-extra py-2"
             >
                 <div
                     style={{ flex: "70%", }}
-                    className="d-flex align-items-center "
+                    className="d-flex align-items-center"
                 >
-                    <div style={{ borderRadius: "50%", aspectRatio: "1/1", height: "80%", background: "lightblue" }}>
+                    <div style={{ borderRadius: "50%", aspectRatio: "1/1", height: "110%", background: "lightblue" }}>
 
                     </div>
                     <span className="text-roboto-700 mx-2">
@@ -32,14 +32,19 @@ const UserCard: React.FunctionComponent<UserCardProps> = (
 
                 <div
                     style={{ flex: "30%" }}
-                    className="d-flex align-items-center"
+                    className="d-flex align-items-center justify-content-end"
                 >
-                    <span>
-                        btn1
-                    </span>
-                    <span className="px-2">
-                        btn2
-                    </span>
+                    <div style={{ borderRadius: "50%", aspectRatio: "1/1", height: "75%", background: "lightgray" }}>
+
+                    </div>
+                    <div className="mx-1">
+
+                    </div>
+                    <div
+                        style={{ borderRadius: "50%", aspectRatio: "1/1", height: "75%", background: "lightgray" }}
+                    >
+
+                    </div>
                 </div>
             </div>
         </>
