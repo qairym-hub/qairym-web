04d31b917771f34e0a8c442e427ab8183fcdb5db
Sham1ko <shamshyrak.zholdasbek@gmail.com>
2022-04-21 18:00:11 +0600

    Работа над стилем круглой кнопки

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,7 @@
 //  buttons
 export { default as DefaultButton } from './buttons/DefaultButton'
 export { default as RedirectButton } from './buttons/RedirectButton'
+export { default as CircleActionButton } from './buttons/CircleActionButton'
 
 //  alerts
 export { default as AlertMessage } from './alerts/AlertMessage'
