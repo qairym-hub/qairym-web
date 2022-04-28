c0bb1cf2307a4e73ef2254dc7b1c15ed470bdb3a
khanZhomart <jtalgatulu@yandex.kz>
2022-04-28 19:02:46 +0600

    Доработка компонентов карточки поста

--- src/pages/home/components/PostCard.tsx
+++ src/pages/home/components/PostCard.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import moment from 'moment'
+import "moment/locale/ru"
+import React, { useState } from 'react'
 import { Badge } from 'react-bootstrap'
 import * as Icon from 'react-bootstrap-icons'
 import { User } from '../../../services/types'
+
+const lorem_text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.  software like Aldus PageMaker including versions of Lorem Ipsum."
 
 interface PostCardProps {
     user: User,
     title: string,
     text: string,
     links: string[],
-    date: string
+    date: string,
+    bg?: string
 }
 
 const PostCard: React.FunctionComponent<PostCardProps> = (
@@ -17,49 +22,120 @@ const PostCard: React.FunctionComponent<PostCardProps> = (
         title,
         text,
         links,
-        date
+        date,
+        bg = 'white'
     }
 ) => {
+    const [open, setOpen] = useState<boolean>(false)
+
+    const getRelativeDate = (date: string) => {
+        moment()
+        return moment(date, "DD.MM.YYYY").fromNow()
+    }
 
     return (
         <>
             <div
-                style={{ width: "400px", backgroundColor: '#F8ECD1' }}
-                className="pt-3 px-4 border rounded-extra"
+                style={{ minHeight: "200px", width: "400px", backgroundColor: bg }}
+                className="pt-4 pb-3 px-4 border rounded-extra"
             >
                 <div>
-                    <div>
-                        <span className="text-roboto-500">
+                    <span className="text-roboto-300 fs-5">
+                        {title}
+                    </span>
+                    <div className="d-flex align-items-center mt-2">
+                        <span
+                            style={{ fontSize: "15px" }}
+                            className="text-roboto-500"
+                        >
                             {user.username}
                         </span>
                         <span
-                            style={{ fontSize: "15px" }}
+                            style={{ fontSize: "10px" }}
                             className="text-muted mx-2"
                         >
-                            • {date}
+                            • {getRelativeDate(date)}
                         </span>
                     </div>
-                    <div>
-                        <Badge
-                            pill
+                    <div className="d-flex align-items-start">
+                        <span
+                            style={{ fontSize: "10px" }}
+                            className="text-muted"
                         >
-                            категория
-                        </Badge>
-                        <div className="mx-1 d-inline"></div>
-                        <Badge
-                            pill
+                            гум. помощь
+                        </span>
+                        <span
+                            style={{ fontSize: "10px" }}
+                            className="text-roboto-500 text-muted mx-1"
                         >
-                            мероприятия
-                        </Badge>
+                            /
+                        </span>
+                        <span
+                            style={{ fontSize: "10px" }}
+                            className="text-muted"
+                        >
+                            сборы
+                        </span>
                     </div>
                 </div>
-                <div className="mt-4">
-                    <span className="text-roboto-300">
-                        {title}
-                    </span>
-                    <p className="mt-2">
-                        {text}
-                    </p>
+                <div>
+                    {open ? (
+                        <div>
+                            <p className="mt-2">
+                                {lorem_text} {text}
+                            </p>
+                            <div>
+                                {links.map((link) => {
+                                    return (
+                                        <div>
+                                            <span>
+                                                <Icon.Link45deg
+                                                    color='gray'
+                                                    size="20"
+                                                />
+                                            </span>
+                                            <div className="d-inline mx-1"></div>
+                                            <span
+                                                style={{ fontSize: "15px" }}
+                                                className="text-muted"
+                                            >
+                                                {link}
+                                            </span>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                            <div className="mt-3">
+                                <span>
+                                    <Icon.ArrowsCollapse
+                                        color='gray'
+                                        size="15"
+                                    />
+                                </span>
+                                <div className="d-inline mx-1"></div>
+                                <span
+                                    style={{ fontSize: "15px", cursor: "pointer" }}
+                                    className="text-muted"
+                                    onClick={() => setOpen(false)}
+                                >
+                                    Свернуть
+                                </span>
+                            </div>
+                        </div>
+                    ) : (
+                        <div
+                            style={{ cursor: "pointer" }}
+                            className="text-muted mt-2"
+                        >
+                            <span
+                                style={{ fontSize: "15px" }}
+                                className=""
+                                onClick={() => setOpen(true)}
+                            >
+                                Подробнее...
+                            </span>
+                        </div>
+                    )}
                 </div>
             </div>
         </>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
 import PostCard from './components/PostCard'
 import UserCard from './components/UserCard'
 
@@ -16,20 +16,69 @@ const HomePage: React.FunctionComponent = () => {
                             username="talgatulyzh"
                         />
                     </div>
-                    <div>
+                    <div className="mt-5">
                         <p className="text-roboto-700 fs-5 mb-2">Моя лента</p>
                         <div>
-                            <PostCard
-                                user={{
-                                    username: 'sham1ko',
-                                    photo_url: 'none',
-                                    location: 'Nur-Sultan'
-                                }}
-                                title="Заголовок #1"
-                                text="Тело"
-                                links={["t.me/sham1ko"]}
-                                date={"25.05.2022"}
-                            />
+                            <Container>
+                                <Row lg={3}>
+                                    <Col className="mt-4">
+                                        <PostCard
+                                            user={{
+                                                username: 'aitu_charity',
+                                                photo_url: 'none',
+                                                location: 'Nur-Sultan'
+                                            }}
+                                            title="Сбор средств для ремонта детского дома"
+                                            text="Тело"
+                                            links={["t.me/charity_meeting", "instagram.com/char_meet"]}
+                                            date={"25.04.2022"}
+                                            bg="#DAE5D0"
+                                        />
+                                    </Col>
+                                    <Col className="mt-4">
+                                        <PostCard
+                                            user={{
+                                                username: 'astanachar',
+                                                photo_url: 'none',
+                                                location: 'Nur-Sultan'
+                                            }}
+                                            title="Субботник на территории Expo"
+                                            text="Тело"
+                                            links={["t.me/charity_meeting"]}
+                                            date={"28.04.2022"}
+                                            bg="#E4D1B9"
+                                        />
+                                    </Col>
+                                    <Col className="mt-4">
+                                        <PostCard
+                                            user={{
+                                                username: 'sham_charity',
+                                                photo_url: 'none',
+                                                location: 'Nur-Sultan'
+                                            }}
+                                            title="Шаму на лечение"
+                                            text="Тело"
+                                            links={["t.me/charity_meeting"]}
+                                            date={"25.04.2022"}
+                                            bg="#BAABDA"
+                                        />
+                                    </Col>
+                                    <Col className="mt-4">
+                                        <PostCard
+                                            user={{
+                                                username: 'sham_charity',
+                                                photo_url: 'none',
+                                                location: 'Nur-Sultan'
+                                            }}
+                                            title="Шаму на лечение"
+                                            text="Тело"
+                                            links={["t.me/charity_meeting"]}
+                                            date={"25.04.2022"}
+                                            bg="#BAABDA"
+                                        />
+                                    </Col>
+                                </Row>
+                            </Container>
                         </div>
                     </div>
                 </Container>
